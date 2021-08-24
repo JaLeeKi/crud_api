d3fa0c0f82b22d2aea6052eb98923f8a963424a5
JaLeeKi <jadekirkham@gmail.com>
2021-08-24 11:29:36 -0600

    uploading Module 2, utilized CRUD application on frontend

--- public/javascripts/delete.js
+++ public/javascripts/delete.js
@@ -1,3 +1,12 @@
+let currentProduct = document.querySelector;
+
+document.getElementById("delete").onclick = (e) => {
+  e.preventDefault();
+  const productId = document.getElementById("product-id").value;
+
+  axios.delete(`/api/products/${productId}`).then(processResult);
+};
+
 function processResult() {
   window.alert("Product deleted!");
 }
